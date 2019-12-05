a8ab39de228bc334fa62b909dd4384ce2561075f
guilherme <jguicf@gmail.com>
2019-12-05 13:11:39 -0300

    Criação prototipos

--- biblio.c
+++ biblio.c
@@ -8,13 +8,35 @@ typedef struct exemplares Exemplares;
 typedef struct livro Livro;
 typedef struct lista ListaLivro;
 
+//metodos para fila de espera
+Espera* criarEspera();
+void inserirEspera(Espera* fila, int matricula);
+int retirarEspera(Espera* fila);
+int esperaVazia(Espera* fila);
+void liberarEspera(Espera* fila);
+
+
+
+//metodos para pilha de exemplares
+Exemplares* criarExemplares();
+void pushExemplar(Exemplares* pilha, int exemplares);
+int popExemplar(Exemplares* pilha);
+int exemplarVazia(Exemplares* pilha);
+void liberarExemplares(Exemplares* pilha);
+
+
+//metodos para lista de livros
+ListaLivro* criarListaLivro();
+void inserirLivro_Ordenado(ListaLivro* livros, int anoPublicaco, char* autor, char* titulo, int qtdExemplares);
+Livro* buscaLivro(ListaLivro* livros, char* titulo);
+void liberarListaLivro(ListaLivro* livros);
+
+
 
 struct matricula{
   int matricula;
   Matricula* prox;
 };
-
-
 struct espera{
   Matricula* inicio;
   Matricula* fim;
